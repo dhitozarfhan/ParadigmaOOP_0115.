47ba2bb2f2f0d8ae137edf102779fdd981475dc2
dhitozarfhan <dhitozarfhan@gmail.com>
2024-05-20 11:11:43 +0700

    commit ke 7

--- pertama 1/pertama 1.cpp
+++ pertama 1/pertama 1.cpp
@@ -1,27 +1,12 @@
 #include <iostream>
 #include <string>
-using namespace std;
+   using namespace std;
 
-class mahasiswa {
-private:
-	const int id;
-	string nama;
-	float nilai;
-
-public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama) {
-
-	}
-
-	~mahasiswa() {
-		cout << "Id   = " << id << endl;
-		cout << "Nama = " << nama << endl;
-		cout << "Nilai =" << nilai << endl;
-	}
-};
+#include "jantung.h"
+#include "manusia.h"
 
    int main() {
-	mahasiswa mhs(12, "Asroni", 90.5);
-
-	return 0;
- }
+	   manusia* varManusia = new manusia("jono");
+	   delete varManusia;
+	   return 0;
+   }
